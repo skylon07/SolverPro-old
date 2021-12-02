8166fadf2a5c8388e18b44bdf93211cb16be6c48
Taylor Brown <taylor@davtar.org>
2021-12-01 19:43:46 -0700

    Improve immutable's workings and implimentations
    This commit also renames "Evaluable" terms to "Substitutable" terms

--- engine.py
+++ engine.py
@@ -16,7 +16,7 @@ class Symbolable:
 
 
 # mixin class that provides an evaluation interface
-class Evaluable:
+class Substitutable:
     def __add__(self, other):
         return Expression(self, '+', other)
 
@@ -36,44 +36,41 @@ class Evaluable:
         return NegativeExpression(self)
 
     @abstractmethod
-    # substDict[symbol] -> numeric value
-    def evaluate(self, substDict):
-        return # numberic value
+    # substDict[symbol] -> Substitutable() or numeric
+    def substitute(self, substDict):
+        return # Substitutable() or numeric
 
 
-# decorator (for classes) that utilizes memory optimizations
-# (should obviously me used only for classes
+# decorator that utilizes memory optimizations
+# (should obviously me used only for classes/functions
 # whose results can be treated as "immutable")
 class immutable:
-    __immutables = dict()
+    _immutables = dict()
 
-    def __init__(self, cls):
-        self.__cls = cls
-
-    def __call__(self, *args, **kwargs):
-        return self.__getMemo(self.__cls, args, kwargs)
-
-    # a decorator that memoizes values from a function/class method
-    @classmethod
-    def memoize(cls, fn):
-        def newFn(*args, **kwargs):
-            return cls.__getMemo(fn, args, kwargs)
-        return newFn
-
-    @classmethod
-    def __getMemo(cls, obj, args, kwargs):
-        memoKey = cls.__makeCallId(obj, args, kwargs)
-        if memoKey not in cls.__immutables:
-            cls.__immutables[memoKey] = obj(*args, **kwargs)
-        return cls.__immutables[memoKey]
+    # ran when python "calls the decorator"
+    def __new__(cls, anyCallable):
+        isClass = type(anyCallable) is type
+        if isClass:
+            class ImmutableMixin(anyCallable):
+                def __new__(mixinCls, *args, **kwargs):
+                    return cls._getMemo(anyCallable, args, kwargs)
+            return ImmutableMixin
+        else:
+            def memoWrapper(*args, **kwargs):
+                return cls._getMemo(anyCallable, args, kwargs)
+            return memoWrapper
 
     @classmethod
-    def __makeCallId(cls, obj, args, kwargs):
-        return str(obj) + str(args) + str(kwargs)
+    def _getMemo(cls, someCallable, argsTuple, kwargsDict):
+        memoKey = cls._makeCallId(someCallable, argsTuple, kwargsDict)
+        if memoKey not in cls._immutables:
+            cls._immutables[memoKey] = someCallable(*argsTuple, **kwargsDict)
+        return cls._immutables[memoKey]
 
-    def __getattr__(self, name):
-        # allows access to original class properties
-        return getattr(self.__cls, name)
+    @classmethod
+    def _makeCallId(cls, someCallable, argsTuple, kwargsDict):
+        return str(id(someCallable)) + str(argsTuple) + str(kwargsDict)
+    
     
 
 # represents the list of all solutions for variables in a single relation
@@ -82,7 +79,7 @@ class Solution():
     def __init__(self, relation):
         pass
     
-    @immutable.memoize
+    @immutable
     def solveVar(symbol):
         pass
 
@@ -90,9 +87,14 @@ class Solution():
 # data structure for two equal expressions
 @immutable
 class Relation(Symbolable):
-    def __init__(self, expr1, expr2):
-        self._leftExpr = expr1
-        self._rightExpr = expr2
+    def __init__(self, subsable1, subsable2):
+        if not isinstance(subsable1, Substitutable):
+            raise TypeError("first argument for Relation must be a substitutable")
+        if not isinstance(subsable2, Substitutable):
+            raise TypeError("second argument for Relation must be a substitutable")
+        
+        self._leftExpr = subsable1
+        self._rightExpr = subsable2
 
     @property
     def left(self):
@@ -103,7 +105,7 @@ class Relation(Symbolable):
         return self._rightExpr
     
     # returns symbol that is assumed equal to zero
-    @immutable.memoize
+    @immutable
     def asSymbol(self):
         return self._leftExpr - self._rightExpr
 
@@ -122,28 +124,29 @@ OPERATORS = {
 }
 # data structure for performing operations
 @immutable
-class Expression(Symbolable, Evaluable):
-    def __init__(self, evalable1, oper, evalable2):
-        if not isinstance(evalable1, Evaluable):
-            raise TypeError("first argument for Expression must be an evaluable")
+class Expression(Symbolable, Substitutable):
+    def __init__(self, subsable1, oper, subsable2):
+        if not isinstance(subsable1, Substitutable):
+            raise TypeError("first argument for Expression must be a substitutable")
         if oper not in OPERATORS:
             raise TypeError("second argument for Expression must be an operator")
-        if not isinstance(evalable2, Evaluable):
-            raise TypeError("third argument for Expression must be an evaluable")
-        self._eval1 = evalable1
-        self._eval2 = evalable2
+        if not isinstance(subsable2, Substitutable):
+            raise TypeError("third argument for Expression must be a substitutable")
+        
+        self._subsable1 = subsable1
+        self._subsable2 = subsable2
         self._oper = oper
 
-    @immutable.memoize
+    @immutable
     def asSymbol(self):
-        sym1 = self._eval1.asSymbol()
-        sym2 = self._eval2.asSymbol()
+        sym1 = self._subsable1.asSymbol()
+        sym2 = self._subsable2.asSymbol()
         return self._operate(sym1, sym2)
 
-    @immutable.memoize
-    def evaluate(self, substDict):
-        val1 = self._eval1.evaluate(substDict)
-        val2 = self._eval2.evaluate(substDict)
+    @immutable
+    def substitute(self, substDict):
+        val1 = self._subsable1.substitute(substDict)
+        val2 = self._subsable2.substitute(substDict)
         return self._operate(val1, val2)
 
     def _operate(self, val1, val2):
@@ -159,52 +162,45 @@ class Expression(Symbolable, Evaluable):
             result = val1 ** val2
         else:
             # this should never happen, since oper is validated on construction
-            raise ValueError("Expression tried to evaluate with an invalid operator")
+            raise ValueError("Expression tried to substitute with an invalid operator")
         # resolves floating-point errors
         return round(result, 12)
         
 
 @immutable
-class NegativeExpression(Symbolable, Evaluable):
-    def __init__(self, evalable):
-        if not isinstance(evalable, Evaluable):
-            raise TypeError("first argument for NegativeExpression must be an evaluable")
-        self._eval = evalable
-        self._evalSym = None
-        self._evalEval = None
+class NegativeExpression(Symbolable, Substitutable):
+    def __init__(self, subsable):
+        if not isinstance(subsable, Substitutable):
+            raise TypeError("first argument for NegativeExpression must be a substitutable")
+        
+        self._subsable = subsable
 
-    @immutable.memoize
+    @immutable
     def asSymbol(self):
-        if self._evalSym is not None:
-            return self._evalSym
+        sym = self._subsable.asSymbol()
+        return -sym
 
-        sym = self._eval.asSymbol()
-        self._evalSym = -sym
-        return self._evalSym
-
-    @immutable.memoize
-    def evaluate(self, substDict):
-        if self._evalEval is not None:
-            return self._evalEval
-
-        val = self._eval.evaluate(substDict)
-        self._evalEval = -val
-        return self._evalEval
+    @immutable
+    def substitute(self, substDict):
+        val = self._subsable.substitute(substDict)
+        return -val
 
 
 # data structure for defining (e)value(able)s (identifiers or numbers)
-@immutable
-class Value(Symbolable, Evaluable):
+class Value(Symbolable, Substitutable):
     @abstractmethod
     def __init__(self):
         self._symbol = None # set to sympy-processable value
 
+    @immutable
     def asSymbol(self):
         return self._symbol
 
-    def evaluate(self, substDict):
+    @immutable
+    def substitute(self, substDict):
         return substDict[self._symbol]
 
+@immutable
 class NumericValue(Value):
     def __init__(self, number):
         validTypes = (float, int, str)
@@ -213,6 +209,7 @@ class NumericValue(Value):
             
         self._symbol = float(number)
 
+@immutable
 class IdentifierValue(Value):
     def __init__(self, identifier):
         if type(identifier) is not str:
