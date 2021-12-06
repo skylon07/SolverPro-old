cef90a0d82ddd161bcb9b34bec83c9c93ca699cb
Taylor Brown <taylor@davtar.org>
2021-12-05 19:17:39 -0700

    Improve immutable decorator

--- engine.py
+++ engine.py
@@ -41,19 +41,33 @@ class Substitutable:
         return # Substitutable() or numeric
 
 
-# decorator that utilizes memory optimizations
 # (should obviously me used only for classes/functions
 # whose results can be treated as "immutable")
 class immutable:
     _immutables = dict()
+    _immutableClasses = list()
 
     # ran when python "calls the decorator"
     def __new__(cls, anyCallable):
         isClass = type(anyCallable) is type
         if isClass:
             class ImmutableMixin(anyCallable):
+                # we want "calls" to the class to run the memo function
                 def __new__(mixinCls, *args, **kwargs):
-                    return cls._getMemo(anyCallable, args, kwargs)
+                    return cls._getMemo(mixinCls, args, kwargs, create=mixinCls.__createNew)
+
+                def __init__(self, *args, **kwargs):
+                    # DONT call parent init after __new__; we want to have control of that
+                    return
+
+                # the creation function is separate to avoid recursion errors
+                # (and also gives a constant reference for memoizing)
+                @classmethod
+                def __createNew(mixinCls, *args, **kwargs):
+                    newObj = super(ImmutableMixin, mixinCls).__new__(mixinCls)
+                    super(mixinCls, newObj).__init__(*args, **kwargs)
+                    return newObj
+            cls._immutableClasses.append(ImmutableMixin)
             return ImmutableMixin
         else:
             def memoWrapper(*args, **kwargs):
@@ -61,15 +75,27 @@ class immutable:
             return memoWrapper
 
     @classmethod
-    def _getMemo(cls, someCallable, argsTuple, kwargsDict):
+    def _getMemo(cls, someCallable, argsTuple, kwargsDict, create=None):
+        if len(kwargsDict) > 0:
+            # currently no way to distinguish if an argument becomes a keyword argument
+            # def fn(arg, kwargs="val")...
+            # fn(1, 2) <-- args=(1,2) kwargs={}
+            raise ValueError("Immutables cannot be created/called with keyword arguments")
+        
+        # separate creation function is needed for class wrapping
+        if create is None:
+            create = someCallable
         memoKey = cls._makeCallId(someCallable, argsTuple, kwargsDict)
         if memoKey not in cls._immutables:
-            cls._immutables[memoKey] = someCallable(*argsTuple, **kwargsDict)
+            cls._immutables[memoKey] = create(*argsTuple, **kwargsDict)
         return cls._immutables[memoKey]
 
     @classmethod
     def _makeCallId(cls, someCallable, argsTuple, kwargsDict):
-        return str(id(someCallable)) + str(argsTuple) + str(kwargsDict)
+        callHash = hash(someCallable)
+        argsHash = repr(argsTuple)
+        # kwargsHash ignored; kwargs not currently allowed
+        return hash((callHash, argsHash))
     
     
 
