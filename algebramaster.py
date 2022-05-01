5c2b9e19a81e5dcdc94e0f6ec56096e3e424dc59
Taylor Brown <taylor@davtar.org>
2022-04-30 23:50:42 -0600

    WIP -- Implement new substitution methods
    Some methods are not yet implemented. There are also many TODOs to still
    complete.

--- algebramaster.py
+++ algebramaster.py
@@ -14,10 +14,8 @@ class AlgebraMaster:
             return SubSet({expr})
         
         assert isinstance(expr, sympy.Expr), "Can only substitute for Sympy expressions"
-        return SubSet({
-            self._subUntilFixed(expr, subCombo)
-            for subCombo in self._subCombos()
-        })
+        usedSymbols = set()
+        return self._subToNumericIfPossible(expr, usedSymbols)
 
     def define(self, symbols, vals):
         assert type(symbols) in (tuple, list), "define() requires list or tuple of symbols"
@@ -78,6 +76,56 @@ class AlgebraMaster:
     def exists(self, symbol):
         pass # TODO
 
+    # substitution helper methods
+    def _subToNumericIfPossible(self, expr, usedSymbols):
+        assert isinstance(expr, sympy.Expr), "Can only substitute for Sympy expressions"
+        exprSubSet = SubSet({expr})
+        for symbolToSub in expr.free_symbols:
+            if symbolToSub in usedSymbols:
+                continue
+            usedSymbols.add(symbolToSub)
+
+            symbolNumericSet = self._subSymToNumericIfPossible(symbolToSub, usedSymbols)
+            assert symbolNumericSet.isNumeric, "symbolNumericSet must be a numeric SubSet"
+            if len(symbolNumericSet) > 0:
+                exprSubSet = SubSet({
+                    currExpr.subs(symbolToSub, symbolSub)
+                    for currExpr in exprSubSet
+                    for symbolSub in symbolNumericSet
+                })
+            
+            usedSymbols.remove(symbolToSub)
+        return exprSubSet
+
+    def _subSymToNumericIfPossible(self, symbolToSub, usedSymbols):
+        if self.isKnown(symbolToSub):
+            numericSet = self.getKnown(symbolToSub)
+            assert type(numericSet) is SubSet, "getKnown() did not return a SubSet"
+            assert numericSet.isNumeric, "getKnown() returned a SubSet that had non-numerics"
+            return numericSet
+        # DEBUG
+        elif symbolToSub == sympy.Symbol('c'):
+            return SubSet({-1, 2})
+        else:
+            numericSet = SubSet()
+            symbolSubSet = self._getSolutionsForSymbol(symbolToSub)
+            assert symbolSubSet.isExpressionSet, "solutionSet should not contain numerics"
+            for symbolSub in symbolSubSet:
+                isCircSub = False
+                for symbol in symbolSub.free_symbols:
+                    if symbol in usedSymbols:
+                        isCircSub = True
+                        break
+                if not isCircSub:
+                    possibleNumericSet = self._subToNumericIfPossible(symbolSub, usedSymbols)
+                    if possibleNumericSet.isNumeric:
+                        numericSet.addFrom(possibleNumericSet)
+            return numericSet
+
+    def _getSolutionsForSymbol(symbol):
+        pass # TODO
+
+    # TODO: remove unneeded functions below (if they really are unneeded):
     def _subUntilFixed(self, expr, subCombo):
         lastExpr = None
         iters = 0
@@ -95,7 +143,7 @@ class AlgebraMaster:
 
     def _subCombos_nextRec(self, comboDict):
         currSymbol = None
-        for someSymbol in self._definedSubstitutions:
+        for someSymbol in self._subCombos_symbolKeys():
             someSymbolUsedAlready = someSymbol in comboDict
             if not someSymbolUsedAlready:
                 currSymbol = someSymbol
@@ -104,13 +152,32 @@ class AlgebraMaster:
         if currSymbol is None:
             yield comboDict
         else:
-            associatedSet = self.getDefinition(currSymbol)
+            associatedSet = self.getKnown(currSymbol)
             assert type(associatedSet) is SubSet, "A non-SubSet substitution made its way into the AlgebraMaster..."
             for exprSub in associatedSet:
                 comboDict[currSymbol] = exprSub
                 for combo in self._subCombos_nextRec(comboDict):
                     yield combo
+            assert len(associatedSet) > 0, "Cannot have empty numeric set"
             comboDict.pop(currSymbol)
+
+    def _subCombos_symbolKeys(self):
+        for symbolKey in self._definedSubstitutions:
+            yield symbolKey
+        for symbolKey in self._inferredSubstitutions:
+            yield symbolKey
+
+    def _solveSingle(self, leftExpr, rightExpr, forSymbol):
+        assert isinstance(leftExpr, sympy.Expr), "_solve() not given correct arguments (leftExpr)"
+        assert isinstance(rightExpr, sympy.Expr), "_solve() not given correct arguments (rightExpr)"
+        assert isinstance(forSymbol, sympy.Symbol), "_solve() not given correct arguments (forSymbol)"
+
+        eqZeroExpr = leftExpr - rightExpr
+        solutions = sympy.solve(eqZeroExpr, forSymbol)
+        # some assumptions I made after (a little) testing
+        assert type(solutions) is list, "_solve() solutions are given in list format"
+        assert len([sol for sol in solutions if not isinstance(sol, sympy.Expr)]) == 0, "_solve() solutions are all sympy.Expr instances"
+        return solutions
 
 
 class NotANumericException(Exception):
