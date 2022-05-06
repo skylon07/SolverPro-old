d35a8aa4133539f107432c5d383d440f68f59ce2
Taylor Brown <taylor@davtar.org>
2022-05-06 01:25:44 -0600

    WIP -- Relational solving algorithms

--- algebramaster.py
+++ algebramaster.py
@@ -32,6 +32,7 @@ class AlgebraMaster:
         if not subVals.isNumericSet:
             raise NotANumericException()
         self._definedSubstitutions.update({symbol: subVals for symbol in symbols})
+        self._updateInferredSubstitutions()
 
     def getDefinition(self, symbol):
         assert type(symbol) in (sympy.Symbol, Identifier), "getDefinition() can only work for sympy Symbols and Identifiers"
@@ -50,7 +51,7 @@ class AlgebraMaster:
 
         self._relationsEqZero.add(exprEqZero)
         self._relationSymbols.update(exprEqZero.free_symbols)
-        # TODO: update self._inferredSubstitutions
+        self._updateInferredSubstitutions()
 
     def getInference(self, expr):
         assert isinstance(expr, sympy.Expr) or type(expr) is Identifier, "getInference() can only work for sympy Exprs and Identifiers"
@@ -113,6 +114,167 @@ class AlgebraMaster:
 
             usedExprKeys.remove(exprSubKey)
         return exprSubSet
+
+    # relational/solving functions
+    def _updateInferredSubstitutions(self):
+        allSymbolicSubs = dict(self._definedSubstitutions)
+        allSymbolicSubs.update(self._genNumericSolsFromRelations(self._relationsEqZero))
+        self._extrapolateSolutions(allSymbolicSubs)
+        self._inferredSubstitutions = {
+            exprKey: numSubSet
+            for (exprKey, numSubSet) in self._filterNonNumericSubSets(allSymbolicSubs)
+        }
+
+    def _genNumericSolsFromRelations(self, relations):
+        numericSubs = dict()
+        for relation in relations:
+            for negNumeric in (atom for atom in relation.atoms() if isNumeric(atom)):
+                eqToNumeric = -negNumeric
+                solutionSet = SubSet(sympy.solveset(relation, eqToNumeric))
+                for solution in solutionSet:
+                    subSet = numericSubs.get(solution, SubSet())
+                    subSet.add(eqToNumeric)
+                    numericSubs[solution] = subSet
+        return numericSubs
+
+    def _extrapolateSolutions(self, allSymbolicSubs):
+        inferredSubSets = allSymbolicSubs.values()
+        numInferred = sum(len(subSet) for subSet in inferredSubSets)
+        inferredSubsChanged = True
+        iters = 0
+        while inferredSubsChanged:
+            lastNumInferred = numInferred
+            
+            # dictionary changes; we have to memorize the keys to iterate
+            exprKeys = [key for key in allSymbolicSubs.keys()]
+            for exprKey in exprKeys:
+                numericsEqToExprKey = allSymbolicSubs[exprKey]
+                if not numericsEqToExprKey.isNumericSet:
+                    continue
+
+                # TODO: is a SubSet() really necessary here?
+                #       what about other places?
+                # TODO: maybe substitute known values before making exprsToSub
+                exprsToSub = SubSet({exprKey})
+                usedSymbols = set()
+                collapsedExprSet = self._collapseExprToNumerics(exprsToSub, numericsEqToExprKey, allSymbolicSubs, usedSymbols)
+                if __debug__:
+                    if len(collapsedExprSet) > 0 and collapsedExprSet.isNumericSet:
+                        actualSubSet = allSymbolicSubs[exprKey]
+                        assert collapsedExprSet == actualSubSet, "collapsing expression yielded the wrong answer"
+                    else:
+                        assert collapsedExprSet.isExpressionSet, "collapsing expression did not extract solutions correctly"
+
+            inferredSubSets = allSymbolicSubs.values()
+            numInferred = sum(len(subSet) for subSet in inferredSubSets)
+            inferredSubsChanged = numInferred != lastNumInferred
+
+            iters += 1
+            assert iters < 999999, "_extrapolateSolutions() stuck in an infinite loop"
+
+    def _collapseExprToNumerics(self, exprsToSub, exprsEqSubSet, allSymbolicSubs, usedSymbols):
+        assert exprsEqSubSet.isNumericSet, "Cannot collapse expr to numerics if not given the numerics it equals"
+
+        resultSubSet = SubSet()
+        for exprToSub in exprsToSub:
+            if len(exprToSub.free_symbols) == 1:
+                for theOnlySymbol in exprToSub.free_symbols:
+                    symbol = theOnlySymbol
+                for numericForExprToSub in exprsEqSubSet:
+                    relationForExprToSub = exprToSub - numericForExprToSub
+                    symbolNumSubSet = SubSet(sympy.solveset(relationForExprToSub, symbol))
+                    assert symbolNumSubSet.isNumericSet, "SubSet after solving for single variable was not a numeric set"
+                    
+                    if symbol not in allSymbolicSubs:
+                        allSymbolicSubs[symbol] = symbolNumSubSet
+                    else:
+                        possibleNumerics = allSymbolicSubs[symbol]
+                        invalidNumerics = [numeric for numeric in symbolNumSubSet if numeric not in possibleNumerics]
+                        if len(invalidNumerics) > 0:
+                            raise ContradictionException(symbol, possibleNumerics, invalidNumerics)
+                    resultSubSet.addFrom(
+                        exprToSub.subs(symbol, numeric)
+                        for numeric in symbolNumSubSet
+                    )
+            else:
+                for symbolToSub in exprToSub.free_symbols:
+                    if symbolToSub in usedSymbols:
+                        continue
+                    usedSymbols.add(symbolToSub)
+
+                    for subExprsToSub in self._subExprUsingAllRoutesForSymbol(exprToSub, exprsEqSubSet, symbolToSub, allSymbolicSubs):
+                        assert type(subExprsToSub) is SubSet, "Tried to substitute for a symbol, but a SubSet was not returned"
+                        exprSubSet = self._collapseExprToNumerics(subExprsToSub, exprsEqSubSet, allSymbolicSubs, usedSymbols)
+                        allSymbolsSolved = len(exprSubSet) > 0 and exprSubSet.isNumericSet
+                        if allSymbolsSolved:
+                            resultSubSet.addFrom(exprSubSet)
+                            break # to avoid redundantly substituting the same variable again
+
+                    usedSymbols.remove(symbolToSub)
+        return resultSubSet
+
+    def _subExprUsingAllRoutesForSymbol(self, exprToSub, exprEqSubSet, symbolToSub, allSymbolicSubs):
+        allSymbolSubs = {
+            symbol: allSymbolicSubs[symbol]
+            for symbol in allSymbolicSubs
+            if type(symbol) is sympy.Symbol
+        }
+        
+        symbolSubsKnown = symbolToSub in allSymbolicSubs
+        if symbolSubsKnown:
+            usedExprKeys = set()
+            subExprSet = self._recursiveSubstitute(exprToSub, allSymbolSubs, usedExprKeys)
+            assert len([expr for expr in subExprSet if symbolToSub in expr.atoms()]) == 0, "Substituting didn't eliminate variable"
+            yield subExprSet
+        else:
+             # dictionary changes; we have to memorize the keys to iterate
+            exprKeys = [key for key in allSymbolicSubs.keys()]
+            for exprKey in exprKeys:
+                # no point in substituting the symbol if it isn't even in the expression
+                if symbolToSub not in exprKey.free_symbols:
+                    continue
+                exprKeyEqSubSet = allSymbolicSubs[exprKey]
+                
+                symbolSubs = SubSet.join(
+                    self._solveRelationForSymbol(exprKey - numericEqToExprKey, symbolToSub)
+                    for numericEqToExprKey in exprKeyEqSubSet
+                )
+                relationsToSub = {
+                    exprToSub - numericEqToExprToSub
+                    for numericEqToExprToSub in exprEqSubSet
+                }
+                # you know how you need 3 unique equations to solve a syatem of 3 variables?
+                # this is what this is trying to determine...
+                if self._exprProvidesUniqueSolution(symbolSubs, relationsToSub, symbolToSub):
+                    allSymbolSubs[symbolToSub] = allSymbolicSubs[symbolToSub] = symbolSubs
+                    usedExprKeys = set()
+                    subExprSet = self._recursiveSubstitute(exprToSub, allSymbolSubs, usedExprKeys)
+                    assert len([expr for expr in subExprSet if symbolToSub in expr.free_symbols]) == 0, "Substituting didn't eliminate variable"
+                    yield subExprSet
+
+    def _exprProvidesUniqueSolution(self, symbolSolvesForExprInQuestion, baseRelations, symbolSolvedFor):
+        for symbolSub in symbolSolvesForExprInQuestion:
+            for relation in baseRelations:
+                solutionIsRedundant = relation.subs(symbolSolvedFor, symbolSub) == 0
+                if solutionIsRedundant:
+                    return False
+        return True
+
+    def _solveRelationForSymbol(self, relation, symbol):
+        symbolSubs = sympy.solveset(relation, symbol)
+        if type(symbolSubs) is sympy.Complement:
+            # this happens when an expression was originally solved into the denominator;
+            # just ignore the undefined point
+            symbolSubs = symbolSubs.args[0]
+        return SubSet(symbolSubs)
+
+    def _filterNonNumericSubSets(self, subDict):
+        for exprKey in subDict:
+            assert isinstance(exprKey, sympy.Expr), "subDict did not have sympy Expr keys"
+            subSet = subDict[exprKey]
+            assert type(subSet) is SubSet, "subDict did not contain SubSet values"
+            if subSet.isNumericSet:
+                yield (exprKey, subSet)
 
     # TODO: remove unneeded functions below (if they really are unneeded):
     def _subUntilFixed(self, expr, subCombo):
@@ -197,10 +359,80 @@ class AlgebraMaster:
 class NotANumericException(Exception):
     pass # exists just as a type
 
+class ContradictionException(Exception):
+    def __init__(self, expr, validVals, invalidVals):
+        self._expr = expr
+        self._validVals = validVals
+        self._invalidVals = invalidVals
+
 
 if __name__ == "__main__":
     S = sympy.Symbol
     master = AlgebraMaster()
-    print(master.substitute(S('a')))
-    print(master.substitute(S('a') + S('b')))
-    print(master.substitute(S('c') + S('b')))
+    
+    # master._inferredSubstitutions = {
+    #     S('a'): SubSet({1}),
+    #     S('a') + S('b'): SubSet({2}),
+    #     S('c'): SubSet({3}),
+    # }
+    # print(master.substitute(S('a')))
+    # print(master.substitute(S('a') + S('b')))
+    # print(master.substitute(S('c') + S('b')))
+
+    # print((S('a') + 4).atoms())
+    # print((S('a') - 4).atoms())
+    # print((S('a') + -4).atoms())
+    # print((S('a') + -4).subs(4, 3))
+    # print((S('a') + -4).subs(-4, 3))
+
+    # print(sympy.solveset(S('a') + S('b') + S('c'), S('a')))
+    # print(sympy.solveset(S('a') + S('b') + S('c'), S('a') + S('b')))
+    # print(sympy.solveset(S('a') + S('b') + S('c') + 3, 3))
+
+    # sols = SubSet()
+    # sols.add(4)
+    # sols.add(S('a') + 4 - S('a'))
+    # print(4)
+    # print(S('a') + 4 - S('a'))
+    # print(S('a') + 4 - S('a') is 4)
+    # print(S('a') + 4 - S('a') == 4)
+    # print(hash(S('a') + 4 - S('a')) == hash(4))
+    # print(sols)
+
+    # print((S('a') + S('b') - 4).subs(S('a'), 2))
+
+    # print(sympy.solveset(2 / S('a') - 4, 2))
+    
+    # eq1 = S('a') + 2 * S('b') - 5
+    # solve1_1 = [item for item in sympy.solveset(eq1, 2)][0]
+    # master._inferredSubstitutions[solve1_1] = SubSet({2})
+    # solve1_2 = [item for item in sympy.solveset(eq1, 5)][0]
+    # master._inferredSubstitutions[solve1_2] = SubSet({5})
+    # eq2 = 2 * S('a') + S('b') - 4
+    # solve2_1 = [item for item in sympy.solveset(eq2, 2)][0]
+    # master._inferredSubstitutions[solve2_1] = SubSet({2})
+    # solve2_2 = [item for item in sympy.solveset(eq2, 4)][0]
+    # master._inferredSubstitutions[solve2_2] = SubSet({4})
+    # print(eq1.subs(S('a'), [item for item in sympy.solveset(solve1_1 - 2, S('a'))][0]))
+    # print(eq1.subs(S('a'), [item for item in sympy.solveset(solve1_2 - 5, S('a'))][0]))
+    # # print(eq1.subs(S('a'), [item for item in sympy.solveset(solve2_1 - 2, S('a'))][0]))
+    # print(eq1.subs(S('a'), [item for item in sympy.solveset(solve2_2 - 4, S('a'))][0]))
+    # b = [item for item in sympy.solveset(eq1.subs(S('a'), [item for item in sympy.solveset(solve2_2 - 4, S('a'))][0]), S('b'))][0]
+    # a = [item for item in sympy.solveset(eq1.subs(S('b'), b), S('a'))][0]
+    # print(a, b)
+
+    # print((2 * S('a') + 1/3 + sympy.pi).atoms(sympy.Number, sympy.NumberSymbol))
+
+    # master.relate(S('a') - S('b'), sympy.simplify(1))
+    # master.relate(S('a') + S('b'), sympy.simplify(9))
+
+    (a, b, c) = sympy.symbols("a, b, c")
+    ans = {
+        a: sympy.simplify(4),
+        b: sympy.simplify(7),
+        c: sympy.simplify(-5),
+    }
+    master.relate(a + b + c, ans[a] + ans[b] + ans[c])
+    master.relate(a * c + 2 * b, ans[a] * ans[c] + 2 * ans[b])
+    master.relate(c * (b - a), ans[c] * (ans[b] - ans[a]))
+    master = master
